b39936f64ec568be5a5f8a5416dd3c98ec34bc4e
Münker, Simon <muenker@uni-trier.de>
2024-04-11 17:55:23 +0200

    replace fetch with axios

--- src/api.js
+++ src/api.js
@@ -1,41 +1,27 @@
+import axios from "axios"
+
+const instance = axios.create({
+    baseURL: 'https://inf.cl.uni-trier.de',
+    headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+        "Access-Control-Allow-Origin": "*",
+    }
+});
+
 export function postChat(model, messages) {
-    return fetch("https://inf.cl.uni-trier.de/chat/", {
-        method: "POST",
-        body: JSON.stringify({
-            model: model,
-            messages: messages,
-        }),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        },
-        mode: "cors",
-        cache: "no-cache",
-    })
-        .then((response) => response.json())
+    return instance.post("/chat/", {model: model, messages: messages})
+        .then((response) => response.data)
 }
 
 export function postFeedback(id, content) {
-    return fetch("https://inf.cl.uni-trier.de/feedback/", {
-        method: "POST",
-        body: JSON.stringify({
-            id: id,
-            content: content,
-        }),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        },
-        mode: "cors",
-        cache: "no-cache",
-    })
-        .then((response) => response.json())
+    return instance.post("/feedback/", {id: id, content: content})
+        .then((response) => response.data)
 }
 
 export function getModels() {
-    return fetch("https://inf.cl.uni-trier.de/models/", {method: "GET"})
-        .then((response) => response.json())
+    return instance.get("/models/").then((response) => response.data)
 }
 
 export function getPersonas() {
-    return fetch("https://inf.cl.uni-trier.de/personas/", {method: "GET"})
-        .then((response) => response.json())
+    return instance.get("/personas/").then((response) => response.data)
 }

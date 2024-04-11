1c239c04ec1d5ece1f0268221a430dce5929e052
Münker, Simon <muenker@uni-trier.de>
2024-04-11 13:51:01 +0200

    add feedback, get models endpoint

--- src/api.js
+++ src/api.js
@@ -1,9 +1,10 @@
-export function postInference(model, prompt) {
-    return fetch("https://inf.cl.uni-trier.de/", {
+export function postInference(model, system, messages) {
+    return fetch("https://inf.cl.uni-trier.de/chat/", {
         method: "POST",
         body: JSON.stringify({
             model: model,
-            prompt: prompt,
+            system: system,
+            messages: messages,
         }),
         headers: {
             "Content-type": "application/json; charset=UTF-8"
@@ -11,5 +12,26 @@ export function postInference(model, prompt) {
         mode: "cors",
         cache: "no-cache",
     })
+        .then((response) => response.json())
+}
+
+export function postFeedback(id, content) {
+    return fetch("https://inf.cl.uni-trier.de/feedback/", {
+        method: "POST",
+        body: JSON.stringify({
+            id: id,
+            content: content,
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        },
+        mode: "cors",
+        cache: "no-cache",
+    })
+        .then((response) => response.json())
+}
+
+export function getModels() {
+    return fetch("https://inf.cl.uni-trier.de/models/", {method: "GET"})
         .then((response) => response.json())
 }
